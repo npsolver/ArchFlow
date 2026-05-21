46ba914de0581f7dd22df1308be25231df0348d8
npsolver <raiyanjamil104+5@gmail.com>
2026-05-21 02:06:49 -0400

    set email to localstorage at login and signup

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -7,63 +7,62 @@ import { useState, ChangeEvent, SubmitEvent, useEffect } from "react"
 import { login } from "@/lib/api/auth";
 
 interface FormData {
-	email: string;
-	password: string;
+    email: string;
+    password: string;
 }
 
 export default function Login() {
 
-	const [formData, setFormData] = useState<FormData>({ email: '', password: '' });
+    const [formData, setFormData] = useState<FormData>({ email: '', password: '' });
 
-	// 3. Handle input changes
-	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
-		setFormData({
-			...formData,
-			[name]: value,
-		});
-	};
+    // 3. Handle input changes
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-	// 4. Handle form submission
-	async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
-		event.preventDefault(); // Prevents page reload
+    // 4. Handle form submission
+    async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
+        event.preventDefault(); // Prevents page reload
 
-		try {
-			const fetchedToken = await login(formData.email, formData.password);
-			console.log(fetchedToken)
-			alert("Login successful");
-			redirect("/")
-		} catch (err) {
-			if (isRedirectError(err)) {
-				throw err; // Re-throw the redirect error
-			}
-			console.log(err)
-			alert("Login failed");
-		}
+        try {
+            const fetchedToken = await login(formData.email, formData.password);
+            localStorage.setItem("email", formData.email);
+            alert("Login successful");
+            redirect("/")
+        } catch (err) {
+            if (isRedirectError(err)) {
+                throw err; // Re-throw the redirect error
+            }
+            alert("Login failed");
+        }
 
-	};
+    };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Email:
-				<input
-					type="text"
-					name="email"
-					value={formData.email}
-					onChange={handleInputChange}
-				/>
-			</label>
-			<label>
-				Password:
-				<input
-					type="text"
-					name="password"
-					value={formData.password}
-					onChange={handleInputChange}
-				/>
-			</label>
-			<button type="submit">Submit</button>
-		</form>
-	)
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email:
+                <input
+                    type="text"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="text"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -7,78 +7,77 @@ import { useState, ChangeEvent, SubmitEvent } from "react"
 import { signup } from "@/lib/api/auth";
 
 interface FormData {
-	email: string;
-	password: string;
-	confirmPassword: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
 }
 
 export default function Signup() {
 
-	const [formData, setFormData] = useState<FormData>({ email: '', password: '', confirmPassword: '' });
+    const [formData, setFormData] = useState<FormData>({ email: '', password: '', confirmPassword: '' });
 
-	// 3. Handle input changes
-	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
-		setFormData({
-			...formData,
-			[name]: value,
-		});
-	};
+    // 3. Handle input changes
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-	// 4. Handle form submission
-	async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
-		event.preventDefault(); // Prevents page reload
+    // 4. Handle form submission
+    async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
+        event.preventDefault(); // Prevents page reload
 
-		if (formData.password !== formData.confirmPassword) {
-			alert("The passwords don't match!")
-			return
-		}
+        if (formData.password !== formData.confirmPassword) {
+            alert("The passwords don't match!")
+            return
+        }
 
-		try {
-			const fetchedToken = await signup(formData.email, formData.password);
-			console.log(fetchedToken)
-			alert("Login successful");
-			redirect("/")
-		} catch (err) {
-			if (isRedirectError(err)) {
-				throw err; // Re-throw the redirect error
-			}
-			console.log(err)
-			alert("Login failed");
-		}
+        try {
+            const fetchedToken = await signup(formData.email, formData.password);
+            localStorage.setItem("email", formData.email);
+            alert("Login successful");
+            redirect("/")
+        } catch (err) {
+            if (isRedirectError(err)) {
+                throw err; // Re-throw the redirect error
+            }
+            alert("Login failed");
+        }
 
-	};
+    };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				email:
-				<input
-					type="text"
-					name="email"
-					value={formData.email}
-					onChange={handleInputChange}
-				/>
-			</label>
-			<label>
-				Password:
-				<input
-					type="text"
-					name="password"
-					value={formData.password}
-					onChange={handleInputChange}
-				/>
-			</label>
-			<label>
-				Confirm Password:
-				<input
-					type="text"
-					name="confirmPassword"
-					value={formData.confirmPassword}
-					onChange={handleInputChange}
-				/>
-			</label>
-			<button type="submit">Signup</button>
-		</form>
-	)
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                email:
+                <input
+                    type="text"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="text"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Confirm Password:
+                <input
+                    type="text"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <button type="submit">Signup</button>
+        </form>
+    )
 }
